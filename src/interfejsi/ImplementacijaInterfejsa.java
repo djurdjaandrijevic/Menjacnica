059fa7517a56baf87a9d1c2a2c497fede5391829
djurdja.andrijevic <djurdja.andrijevic@yahoo.com>
2015-03-25 23:34:16 +0100

    Napravljena je nova klasa ImplementacijaInterfejsa koja sadrzi implementirane metode koje su navedene u interfejsu. Metode su sa praznim telom.

---
+++ src/interfejsi/ImplementacijaInterfejsa.java
@@ -0,0 +1,20 @@
+package interfejsi;
+
+import java.util.GregorianCalendar;
+
+public class ImplementacijaInterfejsa {
+	
+	public void dodajKursValuteZaOdredjeniDan(GregorianCalendar datum, String valuta, double prodajni, double srednji, double kupovni){
+		
+	}
+	
+	public void izbrisiKursValuteZaOdredjeniDan(GregorianCalendar datum, String valuta){
+		
+	}
+	
+	public double vratiKursValuteZaOdredjeniDan(GregorianCalendar dan, String valuta){
+	
+	
+	}
+
+}
