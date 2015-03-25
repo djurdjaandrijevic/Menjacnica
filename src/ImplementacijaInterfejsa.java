8e2ceab532e5dd666905223b107359d92908013a
djurdja.andrijevic <djurdja.andrijevic@yahoo.com>
2015-03-26 00:21:44 +0100

    Ispravljena klasa ImplementacijaInterfejsa

---
+++ src/ImplementacijaInterfejsa.java
@@ -0,0 +1,31 @@
+import java.util.GregorianCalendar;
+
+
+
+
+import interfejsi.Menjacnica;
+
+
+
+public class ImplementacijaInterfejsa implements Menjacnica{
+
+
+	
+	public void dodajKursValuteZaOdredjeniDan(GregorianCalendar datum, String val, double prodajni, double srednji, double kupovni){
+	
+		
+			
+	}
+	
+	public void izbrisiKursValuteZaOdredjeniDan(GregorianCalendar datum, String val){
+		
+		
+		
+	}
+	
+	public double[] vratiKursValuteZaOdredjeniDan(GregorianCalendar dan, String val){
+		
+		
+	
+	}
+}
--- src/interfejsi/Menjacnica.java
+++ src/interfejsi/Menjacnica.java
@@ -6,5 +6,5 @@ public interface Menjacnica {
 
 	public void dodajKursValuteZaOdredjeniDan(GregorianCalendar datum, String valuta, double prodajni, double srednji, double kupovni);
 	public void izbrisiKursValuteZaOdredjeniDan(GregorianCalendar datum, String valuta);
-	public double vratiKursValuteZaOdredjeniDan(GregorianCalendar dan, String valuta);
+	public double[] vratiKursValuteZaOdredjeniDan(GregorianCalendar dan, String valuta);
 }
