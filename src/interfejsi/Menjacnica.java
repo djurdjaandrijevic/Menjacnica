0eeed96cb3f30b251e310a94c92d27a655ac5927
djurdja.andrijevic <djurdja.andrijevic@yahoo.com>
2015-03-25 22:33:53 +0100

    Dodat paket interfejsi. Dodat interfejs sa metodama dodajKursValuteZaOdredjeniDan, izbrisiKursValuteZaOdredjeniDan, vratiKursValuteZaOdredjeniDan.

---
+++ src/interfejsi/Menjacnica.java
@@ -0,0 +1,10 @@
+package interfejsi;
+
+import java.util.GregorianCalendar;
+
+public interface Menjacnica {
+
+	public void dodajKursValuteZaOdredjeniDan(GregorianCalendar datum, String valuta, double prodajni, double srednji, double kupovni);
+	public void izbrisiKursValuteZaOdredjeniDan(GregorianCalendar datum, String valuta);
+	public double vratiKursValuteZaOdredjeniDan(GregorianCalendar dan, String valuta);
+}
